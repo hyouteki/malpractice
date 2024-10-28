b40f4cacd0cddf8033b1cc1ec23a1459b0784b89
hyouteki <lakshay21060@iiitd.ac.in>
2024-10-28 15:57:45 +0530

    download mnist dataset if not present in build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,16 @@
 set -xe
 
 CC="gcc"
-DEMO="mnist"
 LIBS="-lm"
 
+if [ ! -d "mnist" ]; then
+	mkdir mnist
+    wget https://github.com/golbin/TensorFlow-MNIST/raw/refs/heads/master/mnist/data/train-images-idx3-ubyte.gz -P mnist
+    gunzip mnist/train-images-idx3-ubyte.gz
+	wget https://github.com/golbin/TensorFlow-MNIST/raw/refs/heads/master/mnist/data/train-labels-idx1-ubyte.gz -P mnist
+    gunzip mnist/train-labels-idx1-ubyte.gz
+fi
+
 mkdir -p ./build
-$CC $DEMO.c -o ./build/$DEMO $LIBS
-./build/$DEMO
+$CC mnist.c -o ./build/mnist $LIBS
+./build/mnist
